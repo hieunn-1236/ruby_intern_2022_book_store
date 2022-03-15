c33e42aa689173d22858b09ad7a891aad7c883a7
kiennt-1235 <ngo.trung.kien@sun-asterisk.com>
2022-03-15 13:58:37 +0700

    Add admin page & login page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     resources :users
     get "/products", to: "pages#products"
+    get "/admin", to: "pages#admin"
+    get "/login", to: "pages#login"
   end
 end
