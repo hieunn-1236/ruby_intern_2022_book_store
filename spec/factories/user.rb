944b46cdb32f64cdb9a6d1b48f526960ce50ebf8
HieuNN <nguyen.ngoc.hieu-b@sun-asterisk.com>
2022-03-31 01:52:45 +0700

    Add Rspec for book controller

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
-  factory:user do
-    name {Faker::Name.name}
-    email {Faker::Internet.email}
-    password {"password"}
-    password_confirmation {"password"}
-    role {0}
-    activated {true}
-    activated_at {Time.zone.now}
+  factory :user do
+    name{Faker::Name.first_name}
+    email{Faker::Internet.email.downcase}
+    password{"password"}
+    password_confirmation{"password"}
+    activated{true}
+    activated_at{Time.zone.now}
   end
 end
